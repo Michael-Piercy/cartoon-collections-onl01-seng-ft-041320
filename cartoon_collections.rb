6ffda84ccb420c5df2d5958e21ed9d01f8b19d68
Michael Piercy <michael.piercy@zoho.com>
2020-04-18 22:50:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,8 @@
 def roll_call_dwarves(array)
   array = ["Doc", "Dopey", "Bashful", "Grumpy"]
   array.each_with_index do |value, index|
-    puts "#{index} #{value}"
+  puts "#{index} #{value}"
+  end
 end
 
 def summon_captain_planet# code an argument here
