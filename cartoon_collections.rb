a61ad07a257d60b1b2d19f62f0c83bca2fed297c
Michael Piercy <michael.piercy@zoho.com>
2020-04-18 23:00:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-  array = ["Doc", "Dopey", "Grumpy" "Bashful"]
+  array = ["Doc", "Dopey", "Grumpy", "Bashful"]
   array.each_with_index do |value, index|
   puts "#{index} #{value}"
   end
