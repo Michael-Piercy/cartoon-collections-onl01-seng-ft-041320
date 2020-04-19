0479861b3d6acf5e096eeb1905471cc667919838
Michael Piercy <michael.piercy@zoho.com>
2020-04-19 00:13:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.each do |planeteer_calls|
+  array.map do |planeteer_calls|
     planeteer_calls.capitalize << "!"
   end
 end
